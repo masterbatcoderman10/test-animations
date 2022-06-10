61e41c6c2b2eabbf3f792bd4f4dae65e59da46bb
MAA089 <maa089@student.bham.ac.uk>
2022-06-11 02:10:18 +0400

    finished animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {motion, useAnimation} from "framer-motion"
+import { useEffect, useState } from 'react';
+import Letter from './components/Letter';
 
 function App() {
+
+  const controls = useAnimation();
+  const [letters, setLetters] = useState(["c", "a", "t", "a", "s", "t", "o", "p", "h", "e"])
+
+  useEffect(() => {
+    controls.set("initial")
+    controls.start("stageOne")
+    .then(setTimeout(() => controls.stop(), 3000))
+    .then(setTimeout(() => controls.start("final"), 3000))
+  })
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        {letters.map(letter => {
+          const randX = getRandomInt(-427, 427);
+          const randY = getRandomInt(215, 400);
+          const angleStart = getRandomInt(-360, 360)
+          const angleMod = getRandomInt(8, 10)
+          let angleEnd;
+          const probability = Math.random()
+
+          probability > 0.5 ? (angleEnd = angleStart + angleMod) : angleEnd = angleStart - angleMod
+
+          return <Letter 
+          alphabet={letter} 
+          controls={controls} 
+          randX={randX} 
+          randY={randY}
+          angleStart={angleStart}
+          angleEnd={angleEnd}
+          />
+        })}
+        {/* <Letter alphabet={"C"} controls={controls}></Letter> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Letter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { motion, useAnimation } from "framer-motion";
+
+const Letter = ({alphabet, controls, randX, angleStart, angleEnd, randY}) => {
+
+    const letterVariantsOne = {
+        initial : {
+          y: randY,
+          x: randX
+        },
+        stageOne : {
+          rotate: [angleStart, angleEnd],
+          transition: {
+              type: "spring",
+              yoyo: Infinity,
+              duration: 0.08
+            }
+        },
+        final : {
+          y:0,
+          x:0,
+          rotate: 0,
+          transition : {
+            duration: 1
+          }
+        }
+      }
+
+
+    return (
+        <motion.div className="letter" variants={letterVariantsOne} animate={controls}>{alphabet}</motion.div>
+    )
+}
+
+export default Letter
